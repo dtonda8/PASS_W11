a0e73873c184965306bdccf369cbc250b4c3fed2
dtonda8 <davidbatonda7@gmail.com>
2024-10-09 08:28:20 +1100

    Add sols

--- Q1.py
+++ Q1.py
@@ -1,8 +1,15 @@
 from data_structures.bst import BinarySearchTree
+from tests.conversions import toBST
 
 def getMinimumDifference(bst: BinarySearchTree) -> int:
-	raise NotImplementedError()
-
+	ans = 10 ** 5 + 1 ## or float("inf") 
+	it = iter(bst)
+	prev_node = next(it)
+	for node in it:
+		diff = node.key - prev_node.key
+		ans = min(ans, diff) ##
+		prev_node = node
+	return ans ## 
 
 if __name__ == "__main__":
-	pass
+	getMinimumDifference(toBST([4,2,6,1,3]))
